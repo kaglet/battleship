1960432f37928d3efa7beff74e0d641012873041
Kago <kmotlhabane769@gmail.com>
2024-09-28 16:53:32 +0200

    Add empty tests for gameboard

---
+++ src/gameboard.test.js
@@ -0,0 +1,19 @@
+const gameboard = require("./gameboard.js");
+
+test("Module exists", () => expect(gameboard).ToBeTruthy());
+
+test("Has specified basic properties/functions) Size", () => {});
+
+test("Board initializes with correct specified size", () => {
+  // check property value pair of module
+});
+
+test("Board initializes with correct specified size", () => {
+  // check property value pair of module
+});
+
+// Place ship, it instantiates, manages, and keeps ships
+// would be nice if they were a self property of the ships though no?
+// We could make it a direct return into ship's properties thereby not making it a side effect, and update frame based off position
+// of all ships and where they are hit
+test("Place ship at specified coordinates");
