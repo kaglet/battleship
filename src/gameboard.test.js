eb755a0479bc6b509d52a97395fa7c101f395d35
Kago <kmotlhabane769@gmail.com>
2024-10-02 17:27:11 +0200

    Try add ship placed out of grid bounds test

--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -19,10 +19,12 @@ test("Board initializes with correct specified size", () => {
 test("Place ship on valid grid space", () => {
   let shipInstance = ship();
   let gameboardInstance = gameboard();
+  let x = 1;
+  let y = 4;
   shipInstance.length = 4;
-  gameboardInstance.placeShip(shipInstance, 1, 4);
-  for (let i = 1; i < 2; i++) {
-    for (let j = 4; j < shipInstance.length; j++) {
+  gameboardInstance.placeShip(shipInstance, x, y);
+  for (let i = x; i < 2; i++) {
+    for (let j = y; j < shipInstance.length; j++) {
       const element = gameboardInstance.grid[i];
       expect(element).toHaveProperty("length");
       expect(element).toHaveProperty("numHits");
@@ -33,4 +35,16 @@ test("Place ship on valid grid space", () => {
   }
 });
 
-// Produce out of bounds error and do not place. I.e. reject placement completely if an out of bounds is detected using the length as a guide.
+// TODO: Produce out of bounds error and do not place. I.e. reject placement completely if an out of bounds is detected using the length as a guide.
+// TODO: There will never be x cells greater than 10 so I do not think you have to ever catch this right?
+test("Place ship on invalid grid space", () => {
+  let shipInstance = ship();
+  let gameboardInstance = gameboard();
+  let x = 1;
+  let y = 10;
+  shipInstance.length = 4;
+  expect(() => gameboardInstance.placeShip(shipInstance, x, y)).toThrow(
+    "Ship is out of bounds and cannot be placed"
+  );
+  // Placement controller should place it in previous spot
+});
