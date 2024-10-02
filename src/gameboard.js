53302741b4db78be6fffcca7670f67b9f933ab6c
Kago <kmotlhabane769@gmail.com>
2024-10-02 17:19:35 +0200

    Add tested place ship on valid grid space

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,15 +1,32 @@
 let gameboard = () => {
   let size = 10;
-  let ships = [];
+  let grid = new Array(size);
+
+  // initialize grid
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      grid[i] = new Array(size);
+    }
+  }
+
+  const placeShip = function (ship, x, y) {
+    for (let i = y; i < y + ship.length; i++) {
+      grid[x][i] = ship;
+    }
+  };
 
   return {
     size,
-    ships,
+    grid,
+    placeShip,
     get size() {
       return size;
     },
     set size(value) {
       size = value;
+    },
+    get grid() {
+      return grid;
     },
   };
 };
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -1,4 +1,5 @@
 const gameboard = require("./gameboard.js");
+const ship = require("./ship.js");
 
 test("Module exists", () => expect(gameboard).toBeTruthy());
 
@@ -6,17 +7,30 @@ test("Has specified basic properties/functions) size, ships", () => {
   // check property value pair of module
   let gameboardInstance = gameboard();
   expect(gameboardInstance).toHaveProperty("size");
-  expect(gameboardInstance).toHaveProperty("ships");
+  expect(gameboardInstance).toHaveProperty("grid");
 });
 
 test("Board initializes with correct specified size", () => {
   // check property value pair of module
   let gameboardInstance = gameboard();
-  expect(gameboardInstance).toHaveProperty("size", 10);
+  expect(gameboardInstance.grid).toHaveProperty("length", 10);
 });
 
-// Place ship, it instantiates, manages, and keeps ships
-// would be nice if they were a self property of the ships though no?
-// We could make it a direct return into ship's properties thereby not making it a side effect, and update frame based off position
-// of all ships and where they are hit
-// test("Place ship at specified coordinates");
+test("Place ship on valid grid space", () => {
+  let shipInstance = ship();
+  let gameboardInstance = gameboard();
+  shipInstance.length = 4;
+  gameboardInstance.placeShip(shipInstance, 1, 4);
+  for (let i = 1; i < 2; i++) {
+    for (let j = 4; j < shipInstance.length; j++) {
+      const element = gameboardInstance.grid[i];
+      expect(element).toHaveProperty("length");
+      expect(element).toHaveProperty("numHits");
+      expect(element).toHaveProperty("isSunk");
+      expect(element).toHaveProperty("hit");
+      expect(element).toHaveProperty("orientation");
+    }
+  }
+});
+
+// Produce out of bounds error and do not place. I.e. reject placement completely if an out of bounds is detected using the length as a guide.
