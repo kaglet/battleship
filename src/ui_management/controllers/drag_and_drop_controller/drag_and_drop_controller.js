8fcb3ea378beb46bc943ec89cd00c79e0e1c3ebb
Kago <kmotlhabane769@gmail.com>
2024-10-05 14:13:37 +0200

    Make copy of test pic move accurately with mouse
    After appearing at coordinates assigned for top and left based off
    absolute position it stops moving if mouse is left. Redrag might be
    possible or simply delete if the picture never made it to a cell.

--- src/ui_management/controllers/drag_and_drop_controller/drag_and_drop_controller.js
+++ src/ui_management/controllers/drag_and_drop_controller/drag_and_drop_controller.js
@@ -8,30 +8,67 @@ const restaurantImg = require("../../../assets/images/ahtziri-lagarde-4_FsMDmCc6
 // One approach is to generate new ship in its place and move the old one (make its position absolute)
 // If dropped before we get to board we must lose it forever
 
-let isDown = false;
-let offset = [0, 0];
+// TODO: Make it redraggable after being dropped or make it disappear, either one
+// TODO: If they find themselves on the grid (within the coordinates, it must be dropped onto the cell mouse is hovering over)
+// If mouse down is clicked while something is being dragged and is currently selected then check if it has been dropped onto a cell
 
 const dragDropController = (() => {
   let ship1;
+  let isDown = false;
+  let offset = [0, 0];
+
+  const createDraggableFromOriginal = function (e, ship1) {
+    let ship1CopyToDrag = document.createElement("img");
+    ship1CopyToDrag.style.backgroundImage = `url("${restaurantImg}")`;
+    ship1CopyToDrag.classList.add("ship", "dragged");
+    ship1CopyToDrag.style.position = "absolute";
+
+    isDown = true;
+    offset = [ship1.offsetLeft - e.clientX, ship1.offsetTop - e.clientY];
+
+    let body = document.querySelector("body");
+    body.appendChild(ship1CopyToDrag);
+
+    console.log(ship1);
+    // Place it at a position when it first appears and as it is dragged it will move
+    // Here it appears on screen directly on top of original pic
+    ship1CopyToDrag.style.left =
+      window.scrollY + ship1.getBoundingClientRect().left + "px";
+    ship1CopyToDrag.style.top =
+      window.scrollX + ship1.getBoundingClientRect().top + "px";
+
+    ship1CopyToDrag.addEventListener(
+      "mousemove",
+      function (e) {
+        e.preventDefault();
+        if (isDown) {
+          mousePosition = {
+            x: e.clientX,
+            y: e.clientY,
+          };
+
+          ship1CopyToDrag.style.left = mousePosition.x + offset[0] + "px";
+          ship1CopyToDrag.style.top = mousePosition.y + offset[1] + "px";
+        }
+      },
+      true
+    );
+  };
+
   const init = function () {
     ship1 = document.querySelector("img");
-    ship1.addEventListener("mousedown", (e) => {
-      let ship1CopyToDrag = document.createElement("img");
-      ship1CopyToDrag.style.backgroundImage = `url("${restaurantImg}")`;
-      ship1CopyToDrag.classList.add("ship", "dragged");
-      ship1CopyToDrag.style.position = "absolute";
-
-      isDown = true;
-      offset = [ship1.offsetLeft - e.clientX, ship1.offsetTop - e.clientY];
-
-      let body = document.querySelector("body");
-      body.appendChild(ship1CopyToDrag);
-
-      // TODO: place it at a position when it first appears and as it is dragged it will move
-      ship1CopyToDrag.style.left = ship1.style.left + 100 + "px";
-      ship1CopyToDrag.style.top = ship1.style.top + 40 + "px";
-    });
+    ship1.addEventListener("mousedown", (e) =>
+      createDraggableFromOriginal(e, ship1)
+    );
   };
+
+  document.addEventListener(
+    "mouseup",
+    function () {
+      isDown = false;
+    },
+    true
+  );
 
   return { init };
 })();
