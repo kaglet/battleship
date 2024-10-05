79a09b530bedd4ede314dc39ca07f568856b9aa6
Kago <kmotlhabane769@gmail.com>
2024-10-05 13:11:47 +0200

    Fix ES module code dictating exports as not CommonJS

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import "./styles/styles.css";
-import pictureDisplay from "./ui_management/components/pictures_display/pictures_display";
+require("./styles/styles.css");
+const pictureDisplay = require("./ui_management/components/pictures_display/pictures_display");
+const dragDropController = require("./ui_management/controllers/drag_and_drop_controller/drag_and_drop_controller.js");
 const main = require("./ui_management/components/main/main.js");
 
 const init = (() => {
@@ -7,4 +8,5 @@ const init = (() => {
   let mainDisplay = main();
   mainDisplay.appendChild(pictureDisplay());
   body.appendChild(mainDisplay);
+  dragDropController.init();
 })();
---
+++ src/ui_management/controllers/drag_and_drop_controller/drag_and_drop_controller.js
@@ -0,0 +1,39 @@
+const restaurantImg = require("../../../assets/images/ahtziri-lagarde-4_FsMDmCc6A-unsplash.jpg");
+
+// TODO: The picture will have to be generated inside to be a child of the grid container in order to specify its placement in the grid.
+// When you demand the pic it must be generated in the pic on drop not necessarily on drag.
+// The dragged picture, the original picture, are all symbolic.
+
+// Manages drag and drop functionality of ship images onto board
+// One approach is to generate new ship in its place and move the old one (make its position absolute)
+// If dropped before we get to board we must lose it forever
+
+let isDown = false;
+let offset = [0, 0];
+
+const dragDropController = (() => {
+  let ship1;
+  const init = function () {
+    ship1 = document.querySelector("img");
+    ship1.addEventListener("mousedown", () => {
+      let ship1CopyToDrag = document.createElement("img");
+      ship1CopyToDrag.style.backgroundImage = `url("${restaurantImg}")`;
+      ship1CopyToDrag.classList.add("ship", "dragged");
+      ship1CopyToDrag.style.position = "absolute";
+
+      isDown = true;
+      offset = [ship1.offsetLeft - e.clientX, ship1.offsetTop - e.clientY];
+
+      let body = document.querySelector("body");
+      body.appendChild(ship1CopyToDrag);
+
+      // TODO: place it at a position when it first appears and as it is dragged it will move
+      ship1CopyToDrag.style.left = ship1.style.left + 40 + "px";
+      ship1CopyToDrag.style.top = ship1.style.top + 40 + "px";
+    });
+  };
+
+  return { init };
+})();
+
+module.exports = dragDropController;
