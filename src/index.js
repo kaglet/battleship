b933a0c967b75fc7dcfea38b502b121a458be053
Kago <kmotlhabane769@gmail.com>
2024-10-07 11:00:09 +0200

    Revert file index.js to commit 4720ef1

--- src/index.js
+++ src/index.js
@@ -2,63 +2,20 @@ require("./styles/styles.css");
 const dragDropController = require("./ui_management/controllers/drag_and_drop_controller/drag_and_drop_controller.js");
 const main = require("./ui_management/components/main/main.js");
 
-var mousePosition;
-var offset = [0, 0];
-var div;
-var isDown = false;
-let container = document.createElement("div");
-container.style.position = "relative";
+const init = (() => {
+  let body = document.querySelector("body");
+  let mainDisplay = main();
 
-div = document.createElement("div");
-div.style.position = "relative";
-div.style.left = "0px";
-div.style.top = "0px";
-div.style.width = "100px";
-div.style.height = "100px";
-div.style.background = "red";
-div.style.color = "blue";
+  body.appendChild(mainDisplay);
+  dragDropController.init();
 
-container.appendChild(div);
-document.body.appendChild(container);
+  let div = document.createElement("div");
+  div.style.position = "absolute";
+  div.style.left = 100 + "px";
+  div.style.top = 100 + "px";
+  div.style.width = "100px";
+  div.style.height = "100px";
+  div.style.backgroundColor = "blue";
 
-// When the mouse is pressed down on the div
-div.addEventListener(
-  "mousedown",
-  function (e) {
-    isDown = true;
-
-    // Calculate the offset between the div's current position and the mouse click position
-    offset = [div.offsetLeft - e.clientX, div.offsetTop - e.clientY];
-  },
-  true
-);
-
-// When the mouse button is released
-document.addEventListener(
-  "mouseup",
-  function () {
-    isDown = false;
-  },
-  true
-);
-
-// When the mouse is moved
-document.addEventListener(
-  "mousemove",
-  function (event) {
-    event.preventDefault();
-
-    if (isDown) {
-      // Get the current mouse position
-      mousePosition = {
-        x: event.clientX,
-        y: event.clientY,
-      };
-
-      // Set the new position of the div by applying the offset
-      div.style.left = mousePosition.x + offset[0] + "px";
-      div.style.top = mousePosition.y + offset[1] + "px";
-    }
-  },
-  true
-);
+  mainDisplay.appendChild(div);
+})();
