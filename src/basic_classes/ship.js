36cf3ef132c5bd12f083c47a824888bbf8518b73
Kago <kmotlhabane769@gmail.com>
2024-10-09 22:40:47 +0200

    Refactor: Use prototype for shared ship instance methods

--- src/basic_classes/ship.js
+++ src/basic_classes/ship.js
@@ -1,12 +1,29 @@
-let ship = () => {
-  let length = 0;
-  let numHits = 0;
-  let orientation = "H";
-
-  const isSunk = function isSunk() {
-    if (numHits === length) {
+const shipProto = {
+  get length() {
+    return this._length;
+  },
+  set length(val) {
+    this._length = val;
+  },
+  get numHits() {
+    return this._numHits;
+  },
+  set numHits(val) {
+    this._numHits = val;
+  },
+  get orientation() {
+    return this._orientation;
+  },
+  setOrientationVertical() {
+    this._orientation = "V";
+  },
+  setOrientationHorizontal() {
+    this._orientation = "H";
+  },
+  isSunk() {
+    if (this._numHits === this._length) {
       return true;
-    } else if (numHits < length) {
+    } else if (this._numHits < this._length) {
       return false;
     } else {
       /* This indicates somehow the number of hits is more than length 
@@ -15,41 +32,22 @@ let ship = () => {
         This affirms to check if the block is not present or is circumvented. */
       throw new Error("Ship cannot be hit multiple times");
     }
-  };
+  },
 
-  const hit = function hit() {
-    numHits++;
-    return numHits;
-  };
+  hit() {
+    this._numHits++;
+    return this._numHits;
+  },
+};
 
-  return {
-    length,
-    numHits,
-    isSunk,
-    hit,
-    orientation,
-    get length() {
-      return length;
-    },
-    set length(val) {
-      length = val;
-    },
-    get numHits() {
-      return numHits;
-    },
-    set numHits(val) {
-      numHits = val;
-    },
-    get orientation() {
-      return orientation;
-    },
-    setOrientationVertical() {
-      orientation = "V";
-    },
-    setOrientationHorizontal() {
-      orientation = "H";
-    },
-  };
+const ship = () => {
+  let newShip = Object.create(shipProto);
+
+  newShip.length = 0;
+  newShip.numHits = 0;
+  newShip.setOrientationHorizontal();
+
+  return newShip;
 };
 
 module.exports = ship;
