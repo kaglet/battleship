2fdde41cf6ebcd56dc4d7e9662afe59ef0ea18f1
Kago <kmotlhabane769@gmail.com>
2024-10-14 19:40:30 +0200

    TODO: Add task for coinciding logical and UI board sizes

--- src/ui_management/components/main/gameboard/gameboard.js
+++ src/ui_management/components/main/gameboard/gameboard.js
@@ -5,6 +5,8 @@ const gameboard = () => {
   boardDisplay.classList.add("board");
   boardDisplay.style.position = "relative";
 
+  /* TODO: Make this based off created logical gameboard so they coincide
+  The dragAndDrop controller must instantiate the gameboard first before this can operate and be called then*/
   for (let i = 0; i < 10; i++) {
     for (let j = 0; j < 10; j++) {
       let cellDisplay = cell();
