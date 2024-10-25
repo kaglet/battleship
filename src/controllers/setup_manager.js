f6380cc6a6dbced0f6b8104e167c23223004b728
Kago <kmotlhabane769@gmail.com>
2024-10-25 09:31:25 +0200

    Feat: On randomize for p1 clear UI board and create new logical board

--- src/controllers/setup_manager.js
+++ src/controllers/setup_manager.js
@@ -1,6 +1,7 @@
 // Pre-game setup
 // Controls what happens before game even starts
 
+const gameboard = require("../basic_classes/gameboard");
 const randomizeShipPlacement = require("../utility_functions/randomize_ships/randomize_ships");
 const gameManager = require("./game_manager");
 
@@ -15,8 +16,10 @@ This is just for setup, define setup */
     // TODO: Remove all ships from selected board for randomization which is the player board of course anyway by default
     // TODO: So randomization working here is fine although you may have to pass parameters for UI and logical board to do randomization on
     randomizeBtn.addEventListener("click", () => {
-      // gameManager.player1.playerGameboard = gameboard();
-      // setupManager.clearUIBoard(gameManager.player1UIBoard);
+      // Delete old and reassign a new gameboard
+      gameManager.player1.playerGameboard = gameboard();
+      // Clear current board for player 1 removing any svgs on grid
+      clearUIBoard(gameManager.player1UIBoard);
       randomizeShipPlacement(
         gameManager.player1UIBoard,
         gameManager.player1.playerGameboard
@@ -25,7 +28,16 @@ This is just for setup, define setup */
   };
 
   // I think when they are destroyed and new instances are created we don't need to manage clearing
-  const clearUIBoard = (board) => {};
+  const clearUIBoard = (board) => {
+    let children = board.children;
+    let childrenArr = Array.from(children);
+
+    childrenArr.forEach((child) => {
+      if (child.classList.contains("ship")) {
+        child.remove();
+      }
+    });
+  };
 
   const populateUIBoardFromLogical = (board) => {};
 
